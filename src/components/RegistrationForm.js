7459fa169ed12e92f58c0bf0994bd384130850c3
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-11 08:19:29 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 export default class RegistrationForm extends React.Component {
@@ -10,4 +11,19 @@ export default class RegistrationForm extends React.Component {
       </form>
     )
   }
+=======
+import React from 'react';
+
+export default class RegistrationForm extends React.Component{
+  render(){
+    return(
+      <div>
+      <form>
+      <input type="text"  />
+      <input type="password" />
+      </form>
+      </div>
+      )
+  }
+>>>>>>> 660f34c17f0dfd4316fe64ff628edd5b95b241d1
 }
--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,14 +1,22 @@
 import React from "react";
+<<<<<<< HEAD
 import FillerText from "./FillerText";
+=======
+>>>>>>> 660f34c17f0dfd4316fe64ff628edd5b95b241d1
 
 export default class Webpage extends React.Component{
   render(){
     return(
+<<<<<<< HEAD
       <div>
       <h1>The world's coolest webpage</h1>
       <FillerText />
       <FillerText />
       </div>
+=======
+      <h1>The world's coolest webpage</h1>
+      
+>>>>>>> 660f34c17f0dfd4316fe64ff628edd5b95b241d1
       )
   }
 }
